c40cded636de541914dcffcebf888ff46931e18a
nishabdShunya <nishantchoubisa92@gmail.com>
2022-11-30 23:24:02 +0530

    created the user model and a dummy user for the project; formed association between user and product model; and used the magic methods that sequelize provide for associations

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user.createProduct({
     title: title,
     imageUrl: imageUrl,
     price: price,
@@ -32,13 +32,13 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   };
   const prodId = req.params.productId;
-  Product.findAll({ where: { id: prodId } })
-    .then(result => {
+  req.user.getProducts({ where: { id: prodId } })
+    .then(products => {
       res.render('admin/edit-product', {
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
         editing: editMode,
-        product: result[0]
+        product: products[0]
       });
     })
     .catch(err => { console.log(err) });
@@ -50,13 +50,13 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedPrice = req.body.price;
   const updatedDesc = req.body.description;
-  Product.findAll({ where: { id: prodId } })
-    .then(result => {
-      result[0].title = updatedTitle;
-      result[0].imageUrl = updatedImageUrl;
-      result[0].price = updatedPrice;
-      result[0].description = updatedDesc;
-      return result[0].save();
+  Product.findOne({ where: { id: prodId } })
+    .then(product => {
+      product.title = updatedTitle;
+      product.imageUrl = updatedImageUrl;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      return product.save();
     })
     .then(result => {
       res.redirect('/admin/products');
@@ -65,7 +65,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
     .then(products => {
       res.render('admin/products', {
         prods: products,
